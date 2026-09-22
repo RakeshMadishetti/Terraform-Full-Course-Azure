cc912c8682c9503ad4b4d1342087bd00e81e0cac
RakeshMadishetti <hartsi.jaan@gmail.com>
2026-09-23 02:21:13 +0530

    prject insertion

--- lessons/day03/main.tf
+++ lessons/day03/main.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
         source = "hashicorp/azurerm"
-        version = "~> 4.8.0"
+        version = "~> 5.5.0"
     }
   }
-  required_version = ">=1.9.0"
+  required_version = ">=1.16.0"
 }
 
 provider "azurerm" {
@@ -16,19 +16,19 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+  name     = "rakeshRG"
+  location = "westindia"
 }
 
 resource "azurerm_storage_account" "example" {
  
-  name                     = "techtutorial101"
+  name                     = "rakesh0sg"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location # implicit dependency
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
   tags = {
-    environment = "staging"
+    environment = "Dev"
   }
 }
--- lessons/day14/provider.tf
+++ lessons/day14/provider.tf
@@ -7,11 +7,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 5.6.0, < 6.0.0"
+      version = ">= 5.6.0"
     }
   }
 
-  required_version = ">= 1.16.3, < 2.0.0"
+  required_version = ">= 1.16.0"
 }
 # configures the provider
 
